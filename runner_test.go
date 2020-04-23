00fc2fdb47046d1e508865c44ee1d5a79d1f55b3
Andrew Thorp <andrew.thorp.dev@gmail.com>
2020-04-22 20:42:20 -0400

    Add break-pause test

--- runner_test.go
+++ runner_test.go
@@ -35,3 +35,32 @@ func TestTaskRunner(t *testing.T) {
 	runner.Toggle()
 	runner.Toggle()
 }
+
+func TestTaskRunnerPauseInBreak(t *testing.T) {
+	baseDir, _ := ioutil.TempDir("/tmp", "")
+	store, err := NewStore(path.Join(baseDir, "pomo.db"))
+	if err != nil {
+		t.Error(err)
+	}
+	err = initDB(store)
+	if err != nil {
+		t.Error(err)
+	}
+	runner, err := NewTaskRunner(&Task{
+		Duration:   time.Second * 2,
+		NPomodoros: 2,
+		Message:    fmt.Sprint("Test Task"),
+	}, store, NoopNotifier{})
+	if err != nil {
+		t.Error(err)
+	}
+
+	runner.Start()
+
+	runner.Toggle()
+	runner.Toggle()
+
+	runner.Toggle()
+	runner.Pause()
+	runner.Toggle()
+}
